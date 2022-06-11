767c10e8221e68c7fbe29eaadfeb286e14160bcd
Ueslei <ueshow.ut@gmail.com>
2022-06-10 21:55:32 -0300

    final do final

--- src/main/java/com/imed/primeiro/projeto/PowerRangersController.java
+++ src/main/java/com/imed/primeiro/projeto/PowerRangersController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value="PowerRangers")
 class PowerRangersController {
 
   private final PowerRangersRepository repository;
